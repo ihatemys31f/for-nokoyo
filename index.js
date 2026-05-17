5224b1681165e702271b365e4cdae57379c0f103
ISOMAN <wiktor@wodzyn.pl>
2026-05-17 12:33:57 +0200

    tests2

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { Client, MessageEmbed, WebEmbed } = require('discord.js-selfbot-v13');
 const client = new Client();
 require('dotenv').config()
 
-
-let bot = "735147814878969968"
+let disboard_bot = "302050872383242240"
+let fibo_bot = "735147814878969968"
 let channel = "1487908618622730430"
-
+let test = "615855594053828628"
 
 
 client.on('ready', async () => {
@@ -14,20 +14,14 @@ client.on('ready', async () => {
 
 client.on("message", async (ctx) => {
     if (ctx.channelId === channel) {
-        if (ctx.author.id === bot) {
-            if (ctx.content === MessageEmbed) {
-                const channel = client.channels.cache.get(ctx.channelId);
-                channel.send("dzialam v1");
-            }
+        if (ctx.author.id === fibo_bot) {
+                //const channel = client.channels.cache.get(ctx.channelId);
+                //channel.sendSlash(disboard_bot, "bump");
 
-            if (ctx.content === WebEmbed) {
+            if (ctx.content.startsWith("<@1289995374953762858>")) {
                 const channel = client.channels.cache.get(ctx.channelId);
-                channel.send("dzialam v2");
+                channel.sendSlash(disboard_bot, "bump");
             }
-
-            const channel = client.channels.cache.get(ctx.channelId);
-            channel.send("kk")
-            
         }
     }
 })
