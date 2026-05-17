3bb5a6b6cb2ccc8b1aa453599d2c9a082a237a36
ISOMAN <wiktor@wodzyn.pl>
2026-05-17 15:44:55 +0200

    small fixes

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ require('dotenv').config()
 
 let disboard_bot = "302050872383242240"
 let fibo_bot = "735147814878969968"
-let channel = "1487908618622730430"
-let test = "615855594053828628"
+let channelId = "1487908618622730430"
 
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
+  const channel = client.channels.cache.get(channelId);
+  channel.send("online!");
 })
 
 client.on("message", async (ctx) => {
-    if (ctx.channelId === channel) {
+    if (ctx.channelId === channelId) {
         if (ctx.author.id === fibo_bot) {
                 //const channel = client.channels.cache.get(ctx.channelId);
                 //channel.sendSlash(disboard_bot, "bump");
@@ -21,6 +22,7 @@ client.on("message", async (ctx) => {
             if (ctx.content.startsWith("<@1289995374953762858>")) {
                 const channel = client.channels.cache.get(ctx.channelId);
                 channel.sendSlash(disboard_bot, "bump");
+                console.log("bump");
             }
         }
     }
